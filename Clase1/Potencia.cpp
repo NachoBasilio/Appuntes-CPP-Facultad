cdf37b5ab47c1582cb20ce6de0b9d64b234e69f2
IgnaDev <arcortoon@gmail.com>
2024-04-29 11:30:26 -0300

    Resto y cociente de enteros

--- Clase1/Potencia.cpp
+++ Clase1/Potencia.cpp
@@ -11,5 +11,8 @@ int main() {
     cin >> num2;
     potencia = powf(num1, num2);
     cout << "La potencia es: " << potencia << endl;
+    system("read -p 'Presiona Enter para continuar...' var");
+    //Como yo estoy en ubuntu no puedo usar system pause, por eso uso system read
+    cout << "Fin del programa" << endl;
     return 0;
 }
