86f9698e522e76d6307c294cf901e69fb2ff3c63
IgnaDev <arcortoon@gmail.com>
2024-04-29 12:04:26 -0300

    factoria

---
+++ Clase1/buclesFor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    int factorial = 1;
+    int i;
+
+    cout << "Ingrese un número: ";
+    cin >> num;
+
+    for (i = 1; i <= num; i++)
+    {
+        factorial = factorial * i;
+    }
+    
+
+
+    cout << "El factorial de " << num << " es: " << factorial << endl;
+    system("read -p 'Presiona Enter para continuar...' var");
+    return 0;
+}
