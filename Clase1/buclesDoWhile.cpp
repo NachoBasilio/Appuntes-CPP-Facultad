d24b04c43acde5d247cf3fdca5d2bd73dec21970
IgnaDev <arcortoon@gmail.com>
2024-04-29 12:51:24 -0300

    Do while bandera

--- Clase1/buclesDoWhile.cpp
+++ Clase1/buclesDoWhile.cpp
@@ -5,13 +5,33 @@ using namespace std;
 int main()
 {
     int num, suma=0;
+    bool cero = false;
 
+    //Centinela
+    // do
+    // {
+    //     cout << "Ingrese un número: (0 para salir)";
+    //     cin >> num;
+    //     suma += num;
+    // } while (num != 0);
+
+    // cout << "La suma de los números ingresados es: " << suma << endl;
+    // system("read -p 'Presiona Enter para continuar...' var");
+    // return 0;
+
+    //Bandera
     do
     {
         cout << "Ingrese un número: (0 para salir)";
         cin >> num;
+
+        if (num == 0)
+        {
+            cero = true;
+        }
+
         suma += num;
-    } while (num != 0);
+    } while (!cero);
 
     cout << "La suma de los números ingresados es: " << suma << endl;
     system("read -p 'Presiona Enter para continuar...' var");
